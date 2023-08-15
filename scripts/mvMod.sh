2ae21d62e49c781d80e8bd8c89ba6c1cfb56d1d5
Satoshi Matsushita <satoshi.matsushita@neuma.tech>
2023-08-15 20:05:29 +0000

    Scripts to rm ^M:   scripts/{mvMod.sh, rmCR.sh}

---
+++ scripts/mvMod.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -f
+
+HOME="/home/matsushi"
+SRC=" ./include/hwlib.h ./include/socal/hps.h ./include/socal/socal.h ./include/socal/alt_gpio.h ./demos/hps_gpio/._main.c ./demos/hps_gpio/Makefile ./demos/hps_gpio/main.c"
+
+for s in $SRC
+do
+  f="$HOME/$s"
+  echo $f
+  mv ${f}.mod $f
+done
+
