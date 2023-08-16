adb68e315a2865e7c0c280077dc5ced7557a2fed
Satoshi Matsushita <satoshi.matsushita@neuma.tech>
2023-08-16 09:31:32 +0000

    Completed poi1 KBD LED:   Makefile  main.c

---
+++ demos/poiLedKbd/main.c
@@ -0,0 +1,187 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "/home/matsushi/include/hwlib.h"
+#include "/home/matsushi/include/socal/socal.h"
+#include "/home/matsushi/include/socal/hps.h"
+#include "/home/matsushi/include/socal/alt_gpio.h"
+
+#define HW_REGS_BASE ( ALT_STM_OFST )
+#define HW_REGS_SPAN ( 0x04000000 )
+#define HW_REGS_MASK ( HW_REGS_SPAN - 1 )
+
+#define USER_IO_DIR     (0x0fc0ff00)	//  Out=GPIO[27:22]=sel[C,B,A, UD,LD], GPIO[15:8]=Seg;   IN=GPIO[3:0];
+#define SEL_MASK		(0x0fc00000)
+#define SEL_LD			(0x00400000)
+#define SEL_UD			(0x00800000)
+#define SEL_138_A		(0x01000000)
+#define SEL_138_B		(0x02000000)
+#define SEL_138_C		(0x04000000)
+#define SEL_138_EN		(0x08000000)
+
+#define RSTIN_MASK      (0x00000008)
+#define KEYIN_MASK      (0x00000007)
+#define CMDIN_MASK      (0x00000004)
+#define UPN_MASK		(0x00000002)
+#define LON_MASK		(0x00000001)
+
+#define MAX_SEL		10		// scans select from 0 to MAX_SEL
+#define DIG_WAIT_US	1000	// wait us between segment/key scan
+
+void *virtual_base;
+void set_sel(int sel) {
+	unsigned int setbits, clrbits;
+	// set sel for segment/key scan selection
+	if (sel == 9) {
+		clrbits = SEL_UD;
+	} else if (sel == 8) {
+		clrbits = SEL_LD;
+	} else {
+		clrbits = SEL_138_EN | ((7 ^ sel) << 24);
+	}
+	setbits = SEL_MASK ^ clrbits;
+	alt_setbits_word( (virtual_base + ((uint32_t)( ALT_GPIO1_SWPORTA_DR_ADDR ) & (uint32_t)( HW_REGS_MASK ))), setbits);
+	alt_clrbits_word( (virtual_base + ((uint32_t)( ALT_GPIO1_SWPORTA_DR_ADDR ) & (uint32_t)( HW_REGS_MASK ))), clrbits);
+}
+
+unsigned char led_seg[10];	// led_seg[10-6] = digit Upper[DP,3,2,1,0]	: segment with is lit
+void led_disp(int sel) {
+	// display LED segments at sel - 0 out to lit the segments: [D15,D14,..., D8] = [DP,G,...,(A/D1)]
+	int segments;
+	unsigned int setbits, clrbits;
+	segments = led_seg[sel] & 0xff;
+	clrbits = segments << 8;
+	setbits = (0xff ^ segments) << 8;
+	alt_setbits_word( (virtual_base + ((uint32_t)( ALT_GPIO1_SWPORTA_DR_ADDR ) & (uint32_t)( HW_REGS_MASK ))), setbits);
+	alt_clrbits_word( (virtual_base + ((uint32_t)( ALT_GPIO1_SWPORTA_DR_ADDR ) & (uint32_t)( HW_REGS_MASK ))), clrbits);
+}
+
+unsigned char mem[65536];	// memory, zero initialized
+unsigned short maddr;		// memory address register
+unsigned short mdata;		// memory data register
+unsigned char   mden;		// mden bit i turns on mdata digit i
+unsigned char	 cmd;		// 0:Ret, ... 4:AdrSet, 5: RdIncr, 6:RdDecr, 7:WrIncr
+
+unsigned char seg_tbl = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7c, 0x07,
+                         // 0,    1,    2,    3,    4,    5,    6,    7, 
+						 0x7f, 0x67, 0x77, 0x7c, 0x58, 0x5e, 0x79, 0x71, 0x80};	// segments for 0, ... 9, DP
+                         // 8,    9,    A,    b,    c,    d,    e,    f, 
+
+void set_ledseg(int data, int ena, int upper) {
+	// set four led segment refering 16 bit data and 4bit ena, if upper=1 set upper 4 digits 
+	int i, d, ofs; 
+	ofs = upper ? 5 : 0;
+	// set segments for lower 4 digits enabled by 1 in ena[3:0]
+	for (i = 0; i < 4; i++) {
+		if (ena & 1) {
+			d = data & 0x0f;
+			led_seg[i + ofs] = seg_tbl[d];	// digit 0 to F
+		} else {
+			led_seg[i + ofs] = seg_tbl[0x10];	// DP
+		}
+		ena >>= 1;
+		data >>= 4;
+	}
+}
+
+void rd_mem(maddr) {
+	unsigned char md;
+	md = mem[maddr];
+	set_ledseg(maddr, 0xf, 1);
+	set_ledseg(md, 3, 0);
+}
+
+void exec_cmd(int cmd) {
+	// execute commands
+	switch (cmd) {
+		case 4:	// Adr Set
+			maddr = mdata;
+			rd_mem(maddr);
+			break;
+		case 5:	// Read Incr
+		case 6: // Read Decr
+			maddr += (cmd == 5) ? 1 : -1;
+			rd_mem(maddr);
+			break;
+		case 7: // Write Incr
+			mem[maddr] = mdata;
+			maddr += 1;
+			rd_mem(maddr);
+			break;
+		default:
+			printf("Cmd=%d not implemented\n", cmd);
+			break;
+	}
+}
+
+int main(int argc, char **argv) {
+	int fd;
+	uint32_t  scan_input;
+	int	sel, key_on, key_prev;
+	int key_val, rst;	// register saving key scan result
+	int i;
+
+	// map the address space for the LED registers into user space so we can interact with them.
+	// we'll actually map in the entire CSR span of the HPS since we want to access various registers within that span
+	if( ( fd = open( "/dev/mem", ( O_RDWR | O_SYNC ) ) ) == -1 ) {
+		printf( "ERROR: could not open \"/dev/mem\"...\n" );
+		return( 1 );
+	}
+
+	virtual_base = mmap( NULL, HW_REGS_SPAN, ( PROT_READ | PROT_WRITE ), MAP_SHARED, fd, HW_REGS_BASE );
+	
+	if( virtual_base == MAP_FAILED ) {
+		printf( "ERROR: mmap() failed...\n" );
+		close( fd );
+		return( 1 );
+	}
+	
+	// initialize the gpio controller
+	printf("Setting GPIO diretion...\n");
+	alt_setbits_word( (virtual_base + ((uint32_t)( ALT_GPIO0_SWPORTA_DDR_ADDR ) & (uint32_t)( HW_REGS_MASK ))), USER_IO_DIR );
+
+	printf("Scan key and LEDs\n");
+	key_prev = 0;	
+	while (1) {
+		for(sel = key_on = 0; sel < MAX_SEL; sel++) {
+			set_sel(sel);
+			led_disp(sel);
+			scan_input = alt_read_word( (virtual_base + ((uint32_t)( ALT_GPIO0_EXT_PORTA_ADDR ) & (uint32_t)( HW_REGS_MASK ))) );		
+			if (i = (scan_input & (RSTIN_MASK|KEYIN_MASK))) {
+				key_on++;
+				key_val = (sel << 4) | i;	// [7:4, 3:0] = [sel, scan_input]
+			}
+			usleep(DIG_WAIT_US);	
+		}
+		// Check if key is pressed. 
+		if (key_prev == key_on) {	// key is stabilized - chattering cancel if current key_on is the same as 16 ms ago.
+			if (key_on) { // Key pressed
+				// Analyze key and execute command
+				i = (key_val >> 4) & 7;
+				if (key_val & RSTIN_MASK) {
+					rst = 1;
+				} else if (key_val & CMDIN_MASK) {
+					exec_cmd(i);
+					// reset mdata, mden for next number imput
+					mdata = mden = 0;
+				} else {
+					i += (key_val & UPN_MASK) ? 8 : 0;	// get tenkey value
+					mdata = (mdata << 4) | i;
+					mden = (mden << 1) | 1;
+					set_ledseg(mdata, mden, 0);		// lower 4 digits
+				}
+			}
+		}
+		key_prev = key_on;
+	}
+
+	// clean up our memory mapping and exit
+	if( munmap( virtual_base, HW_REGS_SPAN ) != 0 ) {
+		printf( "ERROR: munmap() failed...\n" );
+		close( fd );
+		return( 1 );
+	}	
+	close( fd );
+	return( 0 );
+}
